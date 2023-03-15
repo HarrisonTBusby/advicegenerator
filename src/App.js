a18d7c16703abf12e8dabe219bde34f4024d325e
Harrison <Harrisontbusby@gmail.com>
2023-03-15 00:05:07 -0700

    Peerreview

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// Peer Reviewed by Aisha Mashrah
+// I’ll start with saying good job on this project it looks exactly like the original image , the fetch works and that’s the most important part. There was 
+// Unnecessary padding, other than that everything was good so good job.
+
+
 import Box from './Components/boxcomponent.js';
 
 import './App.css';
